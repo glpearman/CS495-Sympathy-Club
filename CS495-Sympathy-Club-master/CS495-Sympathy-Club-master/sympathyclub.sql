839c72325d4dc18b5a57341ba42037ca196b95a1
alaneichelgithub <46540711+alaneichelgithub@users.noreply.github.com>
2019-02-28 16:36:18 -0500

    Add files via upload
    Updated User Interface Design

---
+++ CS495-Sympathy-Club-master/CS495-Sympathy-Club-master/sympathyclub.sql
@@ -0,0 +1,16 @@
+﻿DROP DATABASE IF EXISTS club_memebers;
+
+CREATE DATABASE club_memebers;
+
+USE club_memebers;
+
+CREATE TABLE Club_Members
+(
+   MemberID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   FirstName varchar(30) NOT NULL,
+   LastName varchar(30) NOT NULL,
+   Address varchar(50) NOT NULL,
+   Balance int NOT NULL,
+   LinkedAccounts FOREIGN Key
+    
+)                 ;
